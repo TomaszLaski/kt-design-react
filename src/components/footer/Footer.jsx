d7dd17a87bd69b5703ab5d598b27904418b9f9b6
Kuba Kazmierczak <kbk.kazmierczak@gmail.com>
2023-05-16 16:19:22 +0200

    Fixed footer feature

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Footer.css'
 
 import { AiFillFacebook, AiFillInstagram } from 'react-icons/ai';
 function Footer() {
+    const [isContentEmpty, setIsContentEmpty] = useState(false);
+
+    useEffect(() => {
+        const isContentEmpty = checkIfContentIsEmpty();
+        setIsContentEmpty(isContentEmpty);
+        
+        window.addEventListener('resize', handleContentChange);
+
+        return () => {
+            window.removeEventListener('resize', handleContentChange);
+        };
+    }, []);
+
+    const handleContentChange = () => {
+        const isContentEmpty = checkIfContentIsEmpty();
+        setIsContentEmpty(isContentEmpty);
+    };
+
+    const checkIfContentIsEmpty = () => {
+        return document.body.scrollHeight <= window.innerHeight;
+    };
     return (
         <>
-            <Navbar bg="light" fixed="bottom" variant="light">
+            <Navbar fixed={isContentEmpty ? 'bottom' : ''} bg="light" variant="light">
                 <Container>
+
                     <Navbar.Brand href="#home">Copyright 2023 KT</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home"><AiFillFacebook /></Nav.Link>
@@ -21,4 +44,4 @@ function Footer() {
     )
 }
 
-export default Footer
+export default Footer;
