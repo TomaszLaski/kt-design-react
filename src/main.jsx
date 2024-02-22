0d6e24beb42035a66a180e5e2cb070c505de3935
Tomasz Łaski <tomasz.laski0001@gmail.com>
2024-02-22 21:49:18 +0100

    added nextUI to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { NextUIProvider } from '@nextui-org/react';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+	<React.StrictMode>
+		<NextUIProvider>
+			<App />
+		</NextUIProvider>
+	</React.StrictMode>
+);
